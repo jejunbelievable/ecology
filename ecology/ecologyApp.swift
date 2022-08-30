bd85b5f158acab3a639582d339de62bb0cddab32
jejunkang <jejunbelievable@gmail.com>
2022-08-30 16:42:28 +0900

    Initial Commit

---
+++ ecology/ecologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecologyApp.swift
+//  ecology
+//
+//  Created by Jejun Kang on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct ecologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
